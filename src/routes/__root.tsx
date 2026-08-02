ea4fd4e7f71f344698aedb4c2728b1e3ad6e59dc
mariafernanda2507 <mariafernanda2507@gmail.com>
2026-08-02 14:02:32 -0300

    Move toast para baixo do header (offset) para nao bloquear os botoes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -144,7 +144,7 @@ function RootComponent() {
     <QueryClientProvider client={queryClient}>
       {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
       <Outlet />
-      <Toaster position="top-center" richColors />
+      <Toaster position="top-center" richColors offset={80} />
     </QueryClientProvider>
   );
 }
